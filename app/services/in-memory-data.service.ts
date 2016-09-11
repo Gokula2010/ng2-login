82fa7c4ff7d469e7ec3fececfabaa8d3dbac332b
Gokula2010 <Gokula2010@yahoo.com>
2016-09-11 14:16:23 +0800

    in memory data service added

---
+++ app/services/in-memory-data.service.ts
@@ -0,0 +1,40 @@
+import { InMemoryDbService } from 'angular2-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+    createDb() {
+        let roles = [
+            { id: 1, name: 'admin' },
+            { id: 2, name: 'engineer' }
+        ];
+
+        let users = [
+            { id: 1, username: 'gokul', firstname:'Gokul', lastname: 'Ranga', password: 'gokul', role_id: 1 },
+            { id: 2, username: 'test', firstname:'Test', lastname: 'Test', password: 'gokul', role_id: 2 }
+        ];
+        let heroes = [
+            { id: 11, name: 'Mr. Nice' },
+            { id: 12, name: 'Narco' },
+            { id: 13, name: 'Bombasto' },
+            { id: 14, name: 'Celeritas' },
+            { id: 15, name: 'Magneta' },
+            { id: 16, name: 'RubberMan' },
+            { id: 17, name: 'Dynama' },
+            { id: 18, name: 'Dr IQ' },
+            { id: 19, name: 'Magma' },
+            { id: 20, name: 'Tornado' }
+        ];
+        return { 
+            roles,
+            users,
+            heroes
+        };
+
+        // return {
+        //     roles,
+        //     users
+        // };
+
+
+
+    }
+}
